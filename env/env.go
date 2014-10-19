c197556c16919f9fdb27030b0671ae91fb4db93d
Hyunseok Hwang <fr.hwang@gmail.com>
2014-10-19 22:33:48 +0900

    Add comments

--- env/env.go
+++ env/env.go
@@ -2,6 +2,8 @@ package env
 
 import "os"
 
+// GetOrDefault should return a value from environment variable,
+// but return a default value if variable isn't exist
 func GetOrDefault(name, defaultValue string) string {
 	if value := os.Getenv(name); value != "" {
 		return value
--- files/files.go
+++ files/files.go
@@ -2,11 +2,13 @@ package files
 
 import "os"
 
+// IsNotExist return true if a path is exist
 func IsNotExist(path string) bool {
 	_, err := os.Stat(path)
 	return os.IsNotExist(err)
 }
 
+// IsExist return true if a path isn't exist
 func IsExist(path string) bool {
 	return !IsNotExist(path)
 }
