29ce7b6debf77b1a7cc906cfc35fb7eac030896b
Hyunseok Hwang <fr.hwang@gmail.com>
2015-01-18 09:07:27 +0900

    Fix typo

--- files/files.go
+++ files/files.go
@@ -2,13 +2,13 @@ package files
 
 import "os"
 
-// IsNotExist returns true if a path isn't exist
+// IsNotExist returns true if a path doesn't exist
 func IsNotExist(path string) bool {
 	_, err := os.Stat(path)
 	return os.IsNotExist(err)
 }
 
-// IsExist returns true if a path is exist
+// IsExist returns true if a path exists
 func IsExist(path string) bool {
 	return !IsNotExist(path)
 }
